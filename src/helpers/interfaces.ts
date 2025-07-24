96f181e15f8e82f1a51d785e090230b7947bd70e
sanjaraybekov <sanjaraliaybekov@gmail.com>
2025-07-24 23:24:21 +0500

    DRY

--- src/helpers/interfaces.ts
+++ src/helpers/interfaces.ts
@@ -2,7 +2,17 @@ export interface QueryParams {
   [key: string]: string | number;
 }
 
-export interface Material {
+export interface Totals {
+  remind_start_sum: number;
+  remind_start_amount: number;
+  remind_income_sum: number;
+  remind_income_amount: number;
+  remind_outgo_sum: number;
+  remind_outgo_amount: number;
+  remind_end_sum: number;
+  remind_end_amount: number;
+}
+export interface Material extends Totals {
   category: string | null;
   code: string;
   color: {
@@ -14,27 +24,8 @@ export interface Material {
   min_amount: number | null;
   name: string;
   parent: string | null;
-  remind_end_amount: number;
-  remind_end_sum: number;
-  remind_income_amount: number;
-  remind_income_sum: number;
-  remind_outgo_amount: number;
-  remind_outgo_sum: number;
-  remind_start_amount: number;
-  remind_start_sum: number;
   unit: string;
   width: string;
-}
-
-export interface Totals {
-  remind_start_sum: number;
-  remind_start_amount: number;
-  remind_income_sum: number;
-  remind_income_amount: number;
-  remind_outgo_sum: number;
-  remind_outgo_amount: number;
-  remind_end_sum: number;
-  remind_end_amount: number;
 }
 
 export interface GroupedMaterials {
